3def4b407d6ab1ff6b850c022053fc96242902ca
Alfonsas Juršėnas <alpha358@gmail.com>
2017-07-13 14:22:18 +0300

    run bbz works

--- run_bbz.m
+++ run_bbz.m
@@ -1,9 +1,9 @@
 %================================= Paths ==================================
-addpath(genpath('C:\Users\Alfonsas\Desktop\MATLAB\Fresnel_Diffraction'))
+addpath(genpath('C:\Users\AJursenas\Desktop\Fresnel_Diffraction'))
 
 
 %% ============================ Initial stuff =============================
-plot_name = 'BBZ';
+plot_name = 'BBZ2';
 
 % Limit threads for stability
 % maxNumCompThreads(8);
@@ -21,16 +21,21 @@ paramValues =  containers.Map();
 
 % TODO: make allcomb work at 5
 % Rounding to 5 decimal places due to rounding in allcomb
-paramValues('alpha1') = num2cell( round( linspace(0.1, 1, 20).*pi./180, 5, 'significant') );
-paramValues('n') = num2cell( linspace(10, 200, 20) );
+paramValues('alpha1') = num2cell( round( linspace(0.1, 1, 20), 5, 'significant') );
+paramValues('n') = num2cell( round( linspace(50, 250, 20),  4 )  );
+paramValues('K') = num2cell( [1.6 1.7, 1.8] );
 
 % handle to get cell id for parameters
-cellID = @(keys) find(  contains(paramValues.keys, keys)  );
+cellID = @(keys) find(  ismember(paramValues.keys, keys)  );
 
+% diffract_bbz( n, alpha1, K )
 %% ======================== handle to the plot fn =========================
+% diffract_bbz( n, alpha1, K )
+
 plot_api_handle = @(varargin) diffract_bbz(             ...
-                varargin{  cellID('alpha1')  },         ...   % n1: refractive index 1
-                varargin{  cellID('n')  }              ...   % n2: refractive index 2
+                varargin{  cellID('n')  },              ...   % 
+                varargin{  cellID('alpha1')  },         ...   % 
+                varargin{  cellID('K')  }               ...   % alpha2/alpha1
               );
 
 % plot_api_handle = @(varargin) plot_api(varargin{:});
