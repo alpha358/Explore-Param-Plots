b2bc5c5638b46c6c3c1209e65887812575c33ca9
Alfonsas Juršėnas <alpha358@gmail.com>
2017-09-04 17:01:26 +0300

    blaze gratings GF

--- run_geom_blaze.m
+++ run_geom_blaze.m
@@ -5,11 +5,11 @@ addpath(genpath('C:\Users\Alfonsas\Desktop\MATLAB\Geom-Plate-Diffraction'))
 
 
 %% ============================ Initial stuff =============================
-plot_name = 'BlazeGF_Issamus2';
+plot_name = 'BlazeGF_FocusFrenel';
 
 % Limit threads for stability
 % maxNumCompThreads(8);
-parpool('SpmdEnabled', false)
+% parpool('SpmdEnabled', false)
 
 %%======= Cell arrays determine for all possible parameter values. ========
 
@@ -23,7 +23,6 @@ paramValues =  containers.Map();
 paramValues('retardance') = num2cell( round(linspace(0, 2 ,11), 5, 'significant') );  % focusing angle   % 1
 paramValues('E_in') = {'x', 'y', 'x+iy', 'x-iy'};
 paramValues('b') = num2cell( round(20:10:120, 5, 'significant') );
-paramValues('rotation') = num2cell( round( [ 1:8 ]./4, 5, 'significant') );
 % handle to get cell id for parameters
 cellID = @(keys) find(  contains(paramValues.keys, keys)  );
 
@@ -32,8 +31,7 @@ cellID = @(keys) find(  contains(paramValues.keys, keys)  );
 plot_api_handle = @(varargin) focus_blaze_grating(         ...
                 varargin{  cellID('retardance')  },              ...   % n1: refractive index 1
                 varargin{  cellID('b')  }, ...    
-                varargin{  cellID('E_in')  }, ...
-                varargin{  cellID('rotation')  } ...
+                varargin{  cellID('E_in')  } ...
               );
 
 
